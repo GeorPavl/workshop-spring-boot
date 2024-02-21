abb97a1c93bf74540830bec856fadc55cc7c5dec
giorgospavl <giorgos.pavloglou@infoteam-software.gr>
2024-02-21 12:34:01 +0200

    WS-1: Initialize project

---
+++ workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/WorkshopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package gr.infoteam.workshop_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootApplication.class, args);
+	}
+
+}
---
+++ workshop_spring_boot/src/test/java/gr/infoteam/workshop_spring_boot/WorkshopSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.infoteam.workshop_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

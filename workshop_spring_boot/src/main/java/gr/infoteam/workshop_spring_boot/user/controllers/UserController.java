2017df5102dd737dfe874b84928c0fb5d1c68594
giorgospavl <giorgos.pavloglou@infoteam-software.gr>
2024-02-21 14:57:16 +0200

    WS-4: Create controller using services

--- workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/user/controllers/UserController.java
+++ workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/user/controllers/UserController.java
@@ -1,36 +1,42 @@
 package gr.infoteam.workshop_spring_boot.user.controllers;
 
-import gr.infoteam.workshop_spring_boot.user.User;
 import gr.infoteam.workshop_spring_boot.user.dtos.UserResponseDto;
-import gr.infoteam.workshop_spring_boot.user.repositories.UserRepository;
+import gr.infoteam.workshop_spring_boot.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return ResponseEntity
                 .ok()
-                .body(userRepository.findAll());
+                .body(userService.getAll());
     }
 
-    @GetMapping("/get-all-dtos")
-    public ResponseEntity<List<UserResponseDto>> getAllUsersDtos() {
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable("id")UUID id) {
         return ResponseEntity
                 .ok()
-                .body(userRepository.findAll().stream()
-                        .map(UserResponseDto::new)
-                        .toList());
+                .body(userService.getById(id));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserResponseDto> getUserByEmail(@PathVariable("email") String email) {
+        return ResponseEntity
+                .ok()
+                .body(userService.getByEmail(email));
     }
 }
---
+++ workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/user/controllers/UserControllerV2.java
@@ -0,0 +1,36 @@
+package gr.infoteam.workshop_spring_boot.user.controllers;
+
+import gr.infoteam.workshop_spring_boot.user.User;
+import gr.infoteam.workshop_spring_boot.user.dtos.UserResponseDto;
+import gr.infoteam.workshop_spring_boot.user.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2/users")
+@RequiredArgsConstructor
+public class UserControllerV2 {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity
+                .ok()
+                .body(userRepository.findAll());
+    }
+
+    @GetMapping("/get-all-dtos")
+    public ResponseEntity<List<UserResponseDto>> getAllUsersDtos() {
+        return ResponseEntity
+                .ok()
+                .body(userRepository.findAll().stream()
+                        .map(UserResponseDto::new)
+                        .toList());
+    }
+}

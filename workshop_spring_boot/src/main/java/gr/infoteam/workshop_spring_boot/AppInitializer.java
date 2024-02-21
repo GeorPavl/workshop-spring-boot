4a29eef8d8106e8b83497d3fa525fe4ab7cd12ba
giorgospavl <giorgos.pavloglou@infoteam-software.gr>
2024-02-21 13:13:21 +0200

    WS-2: Create user objects in CLRunner

--- workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/AppInitializer.java
+++ workshop_spring_boot/src/main/java/gr/infoteam/workshop_spring_boot/AppInitializer.java
@@ -1,5 +1,6 @@
 package gr.infoteam.workshop_spring_boot;
 
+import gr.infoteam.workshop_spring_boot.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,20 @@ public class AppInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("Hello World!");
-        log.error("This is an error!");
+        User userWithConstructor = new User(
+                "Mary",
+                "Doe",
+                "mary@mail.com",
+                "Password123!@#"
+        );
+
+        var userWithSetters = new User();
+        userWithSetters.setFirstName("Jim");
+        userWithSetters.setLastName("Public");
+        userWithSetters.setEmail("jim@mail.com");
+        userWithSetters.setPassword("Password123!@#");
+
+        log.info("User with constructor: " + userWithConstructor);
+        log.info("User with setters: " + userWithSetters);
     }
 }

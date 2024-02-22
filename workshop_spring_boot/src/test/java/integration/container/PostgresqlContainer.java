b1b4c7cf84e5fac360df96c4ec7d26ea216f91be
giorgospavl <giorgos.pavloglou@infoteam-software.gr>
2024-02-22 21:20:03 +0200

    WS-6: Add dependency for testContainers, add custom postgresql container

---
+++ workshop_spring_boot/src/test/java/integration/container/PostgresqlContainer.java
@@ -0,0 +1,31 @@
+package integration.container;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgresqlContainer extends PostgreSQLContainer<PostgresqlContainer> {
+
+    private static final String IMAGE_VERSION = "postgres:latest";
+    private static PostgresqlContainer container;
+
+    private PostgresqlContainer() {super(IMAGE_VERSION);}
+
+    public static PostgresqlContainer getInstance() {
+        if (container == null) {
+            container = new PostgresqlContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+}
